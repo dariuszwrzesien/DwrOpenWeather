cce8e4eab48d9a7e480420acd592afb6e0209b0b
Dariusz Wrzesien <dwrzesien@future-processing.com>
2017-02-13 12:58:56 +0100

    added examples

---
+++ examples/weather-basic-medium.php
@@ -0,0 +1,87 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dwr\OpenWeather\Configuration;
+use Dwr\OpenWeather\OpenWeather;
+use Dwr\OpenWeather\Converter\Converter;
+
+$apiKey = getenv('OPEN_WEATHER_API_KEY');
+$openWeatherConfig = new Configuration($apiKey);
+
+$openWeather = new OpenWeather('Weather', $openWeatherConfig);
+$weather = $openWeather->getByCityName('London');
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>DwrOpenWeather widget-1</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="css/weather-basic-medium.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <section class="ow-container">
+        <header>
+            <div class="caption">
+                <div class="location"><span class="bold"><?php echo $weather->cityName() ?></span></div>
+            </div>
+            <div class="cell">
+                <img class="icon"
+                     src="http://openweathermap.org/img/w/<?php echo $weather->icon() ?>.png"
+                     alt="<?php echo $weather->description() ?>">
+            </div>
+            <div class="cell">
+                <div class="temperature"><?php echo Converter::kelvinToCelsius($weather->temp()) ?>&deg;C</div>
+            </div>
+        </header>
+        <table>
+            <tbody>
+            <tr>
+                <td>Location:</td>
+                <td><span class="bold"><?php echo $weather->cityName() ?></span>, <?php echo $weather->country() ?></td>
+            </tr>
+            <tr>
+                <td>Description:</td>
+                <td><?php echo $weather->description() ?></td>
+            </tr>
+            <tr>
+                <td>Temperature:</td>
+                <td><?php echo Converter::kelvinToCelsius($weather->temp()) ?> &deg;C</td>
+            </tr>
+            <tr>
+                <td>Pressure:</td>
+                <td><?php echo $weather->pressure() ?> hPa</td>
+            </tr>
+            <tr>
+                <td>Wind:</td>
+                <td><?php echo $weather->windSpeed() ?> m/s</td>
+            </tr>
+            <tr>
+                <td>Humidity:</td>
+                <td><?php echo $weather->humidity() ?> %</td>
+            </tr>
+            <tr>
+                <td>Sunrise:</td>
+                <td><?php echo Converter::intToDate($weather->sunrise(), 'H:i:s') ?></td>
+            </tr>
+            <tr>
+                <td>Sunset:</td>
+                <td><?php echo Converter::intToDate($weather->sunset(), 'H:i:s') ?></td>
+            </tr>
+            </tbody>
+        </table>
+    </section>
+</body>
+</html>
---
+++ examples/weather-basic-small.php
@@ -0,0 +1,54 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dwr\OpenWeather\Configuration;
+use Dwr\OpenWeather\OpenWeather;
+use Dwr\OpenWeather\Converter\Converter;
+
+$apiKey = getenv('OPEN_WEATHER_API_KEY');
+$openWeatherConfig = new Configuration($apiKey);
+
+$openWeather = new OpenWeather('Weather', $openWeatherConfig);
+$weather = $openWeather->getByCityName('London');
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>DwrOpenWeather widget-1</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link rel="stylesheet" href="css/weather-basic-small.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <section class="ow-container">
+        <header>
+            <img class="icon"
+                 src="http://openweathermap.org/img/w/<?php echo $weather->icon() ?>.png"
+                 alt="<?php echo $weather->description() ?>">
+        </header>
+        <div class="ow-data">
+            <div class="row">
+                <span class="bold green"><?php echo $weather->cityName() ?> <?php echo $weather->country() ?></span>,
+                <span class="bold italic black small-font"><?php echo $weather->description() ?></span>
+            </div>
+            <div class="row">
+                <span class="bold blue-highlighted"><?php echo Converter::kelvinToCelsius($weather->temp()) ?>&deg;C</span>
+                <span class="small-font">pressure:&nbsp;</span><span><?php echo $weather->pressure() ?> hPa</span>,
+                <span class="small-font">humidity:&nbsp;</span><span><?php echo $weather->humidity() ?> %</span>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
